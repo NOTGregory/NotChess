22bad7c5175d5e33ce20938b58a6e798ac6d6681
unknown <gregory.yang21029@gmail.com>
2024-02-01 21:16:27 -0500

    created a list of positions for pieces

--- Not Chess/Code/Version 1/notChessEngine.js
+++ Not Chess/Code/Version 1/notChessEngine.js
@@ -1,31 +1,42 @@
 // creates a JSON object representing a new game in the Not Chess format
 function create() {
-    //try rewriting using for loops - also consider using the map function
+
     let rows = [];
 
-    for(let i = 0; i < 8; i++){
+    for(let i = 0; i < 8; i++) {
+
         rows[i] = [];
-        for(let j = 0; j < 8; j++){
-            if(i >= 2 && i <= 5){
+
+        for(let j = 0; j < 8; j++) {
+
+            if(i >= 2 && i <= 5) {
+
                 rows[i][j] = null;
+                
                 continue;
             }
+
             let type = "pawn";
 
-            if(i == 0 || i == 7){
-                if(j == 0 || j == 7){
+            if(i == 0 || i == 7) {
+
+                if(j == 0 || j == 7) {
                     type = "rook";
                 }
-                if(j == 1 || j == 6){
+
+                if(j == 1 || j == 6) {
                     type = "knight";
                 }
-                if(j == 2 || j == 5){
+
+                if(j == 2 || j == 5) {
                     type = "bishop";
                 }
-                if(j == 3){
+
+                if(j == 3) {
                     type = "queen";
                 }
-                if(j == 4){
+
+                if(j == 4) {
                     type = "king";
                 }
             }
@@ -39,8 +50,11 @@ function create() {
     }
 
     let game = {
-        rows
-    }
+        rows,
+        turn: true,
+        time: null
+    };
+
     return game;
 }
 
